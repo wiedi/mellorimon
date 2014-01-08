04f897504fdfd883a870258a46bb62ad62f85e0e
Sebastian Wiedenroth <wiedi@frubar.net>
2014-01-08 20:21:04 +0100

    sample 10 fresh seconds with mpstat instead of using average since system boot

--- mpstat.js
+++ mpstat.js
@@ -4,10 +4,15 @@ var exec = require('child_process').exec
 
 function parse(stdout) {
 	var stats = {usr: 0, sys: 0, idl: 0, cpus: 0}
+	var seen_header = 0
 	stdout.split('\n').forEach(function(line) {
 		line = line.trim().split(/\s+/)
 		if(line.length != 16) return
-		if(line[0] == 'CPU') return /* skip header */
+		if(line[0] == 'CPU') {
+			seen_header++
+			return
+		}
+		if(seen_header < 2) return /* only count second table */
 		stats.usr += Number(line[12])
 		stats.sys += Number(line[13])
 		stats.idl += Number(line[15])
@@ -17,7 +22,7 @@ function parse(stdout) {
 }
 
 function mpstat(cb) {
-	exec("mpstat", function(err, stdout, stderr) {
+	exec("mpstat 10 2", function(err, stdout, stderr) {
 		if(err) {
 			cb(null, {})
 			return
@@ -26,7 +31,7 @@ function mpstat(cb) {
 		cb(null, {'mpstat': [
 			"graph_title CPU Utilization",
 			"graph_vlabel %",
-			"graph_info Aggregates percentage each CPU spends on user + system + idel time",
+			"graph_info Aggregates percentage each CPU spends on user + system + idel time with mpstat in a 10 second sample set",
 			"graph_args --base 1000 -l 0 -u " + stats.cpus * 100 ,
 			"graph_scale no",
 			"graph_category system",
