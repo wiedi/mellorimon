59bd203c3ba6575a55be04984a805328abff44af
Sebastian Wiedenroth <wiedi@frubar.net>
2017-07-21 16:10:11 +0200

    also monitor actual zfs usage to take reservations into account

--- zpool.js
+++ zpool.js
@@ -3,7 +3,7 @@
 var exec = require('child_process').exec
 var clean_fieldname = require('./util').clean_fieldname
 
-function parse(stdout) {
+function zpool_parse(stdout) {
 	var pools = []
 	stdout.split('\n').forEach(function(line) {
 		line = line.split('\t')
@@ -19,7 +19,24 @@ function parse(stdout) {
 	return pools
 }
 
-function generate_stats(pools) {
+function zfs_list_parse(stdout) {
+	var fs = []
+	stdout.split('\n').forEach(function(line) {
+		line = line.split('\t')
+		if(line.length != 3) return
+		var avail = Number(line[1])
+		var used  = Number(line[2])
+		fs.push({
+			'name':     line[0],
+			'avail':    avail,
+			'used':     used,
+			'capacity': ((used / (used + avail)) * 100) | 0
+		})
+	})
+	return fs
+}
+
+function generate_zpool_stats(pools) {
 	var stats = {}
 	pools.forEach(function(pool) {
 		var name = clean_fieldname(pool.name)
@@ -48,6 +65,31 @@ function generate_stats(pools) {
 	return stats
 }
 
+function generate_zfs_list_stats(fs_data, stats) {
+	fs_data.forEach(function(fs) {
+		var name = clean_fieldname(fs.name)
+		stats['zfslist_' + name] = [
+			"graph_title zfs usage (" + fs.name + ")",
+			"graph_vlabel bytes",
+			"graph_info Space available and used for ZFS",
+			"graph_args --base 1024 --lower-limit 0",
+			"graph_category disk",
+			"used.draw AREA",
+			"used.label used",
+			"used.value " + fs.used,
+			"avail.draw STACK",
+			"avail.label avail",
+			"avail.value " + fs.avail,
+			"capacity.graph no",
+			"capacity.label capacity",
+			"capacity.value " + fs.capacity,
+			"capacity.warning 92",
+			"capacity.critical 98"
+		]
+	})
+	return stats
+}
+
 function zpool(cb) {
 	var cmd = "zpool list -Hpo name,size,allocated,free,capacity"
 	exec(cmd, function(err, stdout, stderr) {
@@ -55,9 +97,26 @@ function zpool(cb) {
 			cb(null, {})
 			return
 		}
-		var pools = parse(stdout)
-		var stats = generate_stats(pools)
-		cb(null, stats)
+		var pool_data = zpool_parse(stdout)
+		var stats = generate_zpool_stats(pool_data)
+
+		var pool_list = []
+		pool_data.forEach(function(x) {
+			pool_list.push(x.name)
+		})
+
+		var zfs_list_cmd = "zfs list -Hpo name,avail,used " + pool_list.join(" ")
+		exec(zfs_list_cmd, function(err, stdout, stderr) {
+			if(err) {
+				cb(null, {})
+				return
+			}
+			var fs_data = zfs_list_parse(stdout)
+			console.log(stdout, fs_data)
+			stats = generate_zfs_list_stats(fs_data, stats)
+			console.log(stats)
+			cb(null, stats)
+		})
 	})
 }
 
