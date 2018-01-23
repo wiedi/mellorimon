ad94e7ac16ecd0aaaf99a04f80c48975746dbbfa
Sebastian Wiedenroth <wiedi@frubar.net>
2018-01-23 14:48:49 +0100

    cpu: move intr time into separate graph
    It is measured independent of kernel/user/idle time.

--- cpu.js
+++ cpu.js
@@ -22,17 +22,17 @@ function Cpu(cb) {
 			stats[k.split('_').pop()] += Number(results[k])
 		})
 
-		var out = [
+		var out_cpu = [
 			"graph_title CPU Usage",
 			"graph_info CPU time used",
 			"graph_args --base 1000",
 			"graph_vlabel cpu time used per ${graph_period}",
 			"graph_category system",
-			"graph_order kernel intr dtrace user idle"
-		]
+			"graph_order kernel user idle"
+		];
 
-		Object.keys(stats).forEach(function(s) {
-			out = out.concat([
+		['kernel', 'user', 'idle'].forEach(function(s) {
+			out_cpu = out_cpu.concat([
 				s + ".label " + s,
 				s + ".min 0",
 				s + ".type DERIVE",
@@ -41,8 +41,25 @@ function Cpu(cb) {
 				s + ".value " + stats[s]
 			])
 		})
-		
-		cb(null, {'cpu': out})
+
+		var out_intr = [
+			"graph_title Interrupt CPU Usage",
+			"graph_info CPU time used",
+			"graph_args --base 1000",
+			"graph_vlabel cpu time used per ${graph_period}",
+			"graph_category system",
+			"graph_order intr",
+			"intr.label intr",
+			"intr.min 0",
+			"intr.type DERIVE",
+			"intr.cdef intr,1000000000,/",
+			"intr.value " + stats["intr"]
+		]
+
+		cb(null, {
+			'cpu': out_cpu,
+			'intr_cpu': out_intr
+		})
 	})
 }
 
